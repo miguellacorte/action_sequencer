ce15cea077d87c526171e2266993425cdedc4796
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-07-28 14:09:00 +0200

    deployment version 1

--- app.js
+++ app.js
@@ -26,4 +26,6 @@ app.use("/api/auth", auth);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+
+
 module.exports = app;
